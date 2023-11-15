4ec4d67dd5eca4994c182d448f552b686b6f1228
EnzoSives <Enzosives7@gmail.com>
2023-11-15 12:34:33 -0300

    Hoy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar CORS
+  app.enableCors({
+    origin: 'http://localhost:8080', // Reemplaza con el origen de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
