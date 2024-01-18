bb126982953c382ae2efd43c442bde1ee5db78f8
EnzoSives <Enzosives7@gmail.com>
2024-01-18 09:40:37 -0300

    Modificaciones 18-1

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,15 @@ import { UsersModule } from './users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      // host: 'bjeea2lcgcdmneaujsib-mysql.services.clever-cloud.com',  // SECRETARIA DB
-      host: 'bct3k8v4ehk7fh77hma9-mysql.services.clever-cloud.com', // PRUEBA DB
+      host: 'bjeea2lcgcdmneaujsib-mysql.services.clever-cloud.com',  // SECRETARIA DB
+      // host: 'bct3k8v4ehk7fh77hma9-mysql.services.clever-cloud.com', // PRUEBA DB
       port: 3306,
-      // username: 'uxejyhsch46jhsem',// SECRETARIA DB
-      username: 'ualk8hbjhiacgall',// PRUEBA DB
-      // password: '2maesP71TZv3rr2KcJrc',// SECRETARIA DB
-      password: '7K2Hpj3YyVI0VaHoUKCm',// PRUEBA DB
-      // database: 'bjeea2lcgcdmneaujsib',// SECRETARIA DB
-      database: 'bct3k8v4ehk7fh77hma9',// PRUEBA DB
+      username: 'uxejyhsch46jhsem',// SECRETARIA DB
+      // username: 'ualk8hbjhiacgall',// PRUEBA DB
+      password: '2maesP71TZv3rr2KcJrc',// SECRETARIA DB
+      // password: '7K2Hpj3YyVI0VaHoUKCm',// PRUEBA DB
+      database: 'bjeea2lcgcdmneaujsib',// SECRETARIA DB
+      // database: 'bct3k8v4ehk7fh77hma9',// PRUEBA DB
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
